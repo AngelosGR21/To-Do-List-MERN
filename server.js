295b79d1b000ca24530a209c507dd36e178c4441
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-16 23:16:42 +0300

    adding database connection

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//routes
+//database
+const connection = require("./db/connection");
 
+//routes
 const tasks = require("./routes/tasks");
 
+//middleware
 app.use("/api/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`server is up and running on port ${port}`);
-});
+//server connection
+const startServer = async () => {
+  try {
+    await connection();
+    app.listen(port, () => {
+      console.log(`server is up and running on port ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+startServer();
